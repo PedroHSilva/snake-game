c4efcd1d6e64b00db85cc8ff0f064f6c4fef0e20
pedrohsilva <pedro_silva@id.uff.br>
2020-10-19 22:50:27 -0300

    adicionado comida e regra para game over

--- script.js
+++ script.js
@@ -45,6 +45,13 @@ function startGame() {
     if(snake[0].y > 15*box && direction == "up") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "down") snake[0].y = 15 * box;
 
+    for(i=1; i<snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval();
+            alert("Game Over");
+        }
+    }
+
     createBG();
     createSnake();
     drawFood();
@@ -57,7 +64,13 @@ function startGame() {
     if(direction == "up") snakeY += box;
     if(direction == "down") snakeY -= box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;  
+    }
+
     let newHead = {
         x: snakeX,
         y: snakeY
